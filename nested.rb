8d70d0d20198476c8c50d11fd0bed275eacaf064
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-23 16:51:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:known_for] += ["Assembly"]
+    programmer_hash[:dennis_ritchie][:known_for].push("Assembly")
     programmer_hash
 
 
